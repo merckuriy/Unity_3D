45c7b082948fa6796891c2b0c869327131d836b9
Viktor <30699669+merckuriy@users.noreply.github.com>
2021-07-20 20:32:59 +0300

    Add files via upload

---
+++ Car in the field/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 100.0f;
+    private float zBound = 6.0f;
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainMovement();
+    }
+
+    // Move the player based on arrow key input
+    void MovePlayer()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    // Prevent the player from leaving the top and bottom of the screen
+    void ConstrainMovement()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.CompareTag("Enemy")){
+            Debug.Log("Player has collided with enemy.");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup")){
+            Destroy(other.gameObject);
+        }
+    }
+}

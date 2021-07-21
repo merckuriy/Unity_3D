09ceea4438be8074ade93fc64de36e4f03d02f19
Viktor <30699669+merckuriy@users.noreply.github.com>
2021-07-21 12:06:54 +0300

    Add files via upload

---
+++ Prototype 5_Bombs&Crates/DifficultyButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    public int difficulty;
+
+    Button button;
+    GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
---
+++ Prototype 5_Bombs&Crates/Target.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
+    private GameManager gameManager;
+    private Rigidbody targetRb;
+    private float minSpeed = 12, maxSpeed = 16, 
+        maxTorque = 10, 
+        xRange = 4, 
+        ySpawnPos = -2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorgue(), RandomTorgue(), RandomTorgue(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        } 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorgue()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

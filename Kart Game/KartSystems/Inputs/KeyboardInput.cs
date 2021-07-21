4ec87cbf145bac10d82ee00226a51508b1083da9
Viktor <30699669+merckuriy@users.noreply.github.com>
2021-07-21 12:40:19 +0300

    Add files via upload

---
+++ Kart Game/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        public string Horizontal = "Horizontal";
+        public string Vertical = "Vertical";
+
+        public override Vector2 GenerateInput() {
+            //Debug.Log("KeyboardInput:" + Input.GetAxis(Horizontal) + " " + Input.GetAxis(Vertical));
+            return new Vector2 {
+                x = Input.GetAxis(Horizontal),
+                y = Input.GetAxis(Vertical)
+            };
+            
+        }
+    }
+}
